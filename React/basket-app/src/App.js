e73a8982bcf66a678c86d7dc921cf6250bd168e9
YagmurrrCan <74114217+YagmurrrCan@users.noreply.github.com>
2022-11-22 04:18:16 +0300

    Product photos add.

---
+++ React/basket-app/src/App.js
@@ -0,0 +1,90 @@
+
+import './App.css';
+import React from 'react';
+import { Container, SimpleGrid, List, ThemeIcon, Stack, Button, Input } from '@mantine/core';
+import Card from "./components/Card"
+import {useState} from 'react';
+import { IconCircleCheck, IconCircleDashed } from '@tabler/icons';
+
+const storeItems = [
+{
+  name:"Keyboard",
+  src: "keyboard",
+  price: 1000
+},
+{
+  name:"Phone",
+  src: "phone",
+  price: 1500
+},
+{
+  name:"Camera",
+  src: "camera",
+  price: 500
+},
+{
+  name:"Joystick",
+  src: "joystick",
+  price: 500
+},
+{
+  name:"Headphone",
+  src: "headphone",
+  price: 500
+},
+{
+  name:"Computer Desk",
+  src: "desk",
+  price: 500
+}
+]
+
+function App() {
+  let [basketItems, setBasketItems] = useState([]);
+  let [searchValue, setSearchValue] = useState("");
+  let filteredItems = basketItems.filter((item) => item.name.toLowerCase().indexOf(searchValue.toLowerCase()) >= 0);
+  // let filteredItems = basketItems.filter((item) => item.name.indexOf("a") >= 0);
+  return (
+    <Container className="App">
+      <SimpleGrid cols={3} className="store">
+        {storeItems.map(({ name, src }) => {
+          return (
+            <Card  
+              key={name} 
+              name={name}
+              src={src} 
+              onAdd={() => setBasketItems([...basketItems, { name }])}
+              
+            />
+          );
+        })}
+      </SimpleGrid> 
+      {searchValue}
+
+      <Input.Wrapper label="Search">
+        <Input 
+          onChange={(e)=> setSearchValue(e.target.value)}
+        />
+      </Input.Wrapper>
+
+      <List
+      className="list"
+      spacing="xs"
+      size="sm"
+      center
+      icon={
+        <ThemeIcon color="teal" size={24} radius="xl">
+          <IconCircleCheck size={16} />
+        </ThemeIcon>
+      }
+    >
+      {filteredItems.map( ({name}, index) => (
+        <List.Item key={index}> {name} </List.Item> 
+      ))}
+      
+    </List>   
+    </Container>
+  );
+}
+
+export default App;
